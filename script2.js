118b9060f95a6af1b6a72e939848c942b94e51e9
Emill0004 <e.s.miller13125@gmail.com>
2023-03-10 21:52:47 -0700

    added if statements to change charTypes array objects to strings if true.

--- script2.js
+++ script2.js
@@ -19,22 +19,35 @@ function writePassword() {
         if (pwdLength < 8) {
             alert("The password must be at least 8 characters long.");
             return;
-          } else if (pwdLength > 128) {
+        } else if (pwdLength > 128) {
             alert("The password must not be over 128 characters long.")
             return;
-          } else if (!/\d/.test(pwdLength)) {
+        } else if (!/\d/.test(pwdLength)) {
             alert("Password does not contain a number.");
             return;
-          }
-          var charTypes = [confirm("Include lowercase characters in the password?")];
-          charTypes.push(confirm("Include uppercase characters in the password?"));
-          charTypes.push(confirm("Include numbers in the password?"));
-          charTypes.push(confirm("Include special characters in the password?"));
-          if (charTypes[0] !== true && charTypes[1] !== true && 
-              charTypes[2] !== true && charTypes[3] !== true) {
-                alert("At least one type of character needs to be selected.");
-                return;
-          }
+        }
+        var charTypes = [confirm("Include lowercase characters in the password?")];
+        charTypes.push(confirm("Include uppercase characters in the password?"));
+        charTypes.push(confirm("Include numbers in the password?"));
+        charTypes.push(confirm("Include special characters in the password?"));
+        if (charTypes[0] !== true && charTypes[1] !== true && 
+            charTypes[2] !== true && charTypes[3] !== true) {
+            alert("At least one type of character needs to be selected.");
+            return;
+        }
+        if (charTypes[0] == true) {
+            charTypes[0] = "lower";
+        }
+        if (charTypes[1] == true) {
+            charTypes[1] = "upper";
+        }
+        if (charTypes[2] == true) {
+            charTypes[2] = "number";
+        }
+        if (charTypes[3] == true) {
+            charTypes[3] = "special";
+        }
+        console.log(charTypes);
     }
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
