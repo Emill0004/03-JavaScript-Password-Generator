18ec2ea63e4e034161c5da24ff9d1ba68276f5ad
Emill0004 <e.s.miller13125@gmail.com>
2023-03-10 21:34:11 -0700

    made script2.js to start the code from scratch.

--- script.js
+++ script.js
@@ -1,38 +1,44 @@
 // Assignment Code
 // These four indexes contain all of the characters that the random number generator will pick to use in the password.
-lowIndex = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q",
+var lowIndex = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q",
 "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-upIndex = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q",
+var upIndex = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q",
 "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-numIndex = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var numIndex = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-spIndex = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "_", "-", "+", "=", "`", "|", "/", "(", ")",
+var spIndex = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "_", "-", "+", "=", "`", "|", "/", "(", ")",
 "{", "}", "[", "]", ":", ";", "<", ">", ",", ".", "?"];
+
+
 
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
   // This declares the varible criteria that will hold the various conditions the password needs to meet.
+  var password = "";
   var criteria = {
-    length: prompt("Enter password length. (must be between 8 and 128 characters long)")
+    pwdLength: prompt("Enter password length. (must be between 8 and 128 characters long)")
   }
-  // This if statement checks if criteria.length is between 8 and 128 characters long and stops the function generatePassword
-  // if criteria.length is smaller than 8 or bigger than 128.
-  if (criteria.length < 8) {
+  // This if statement checks if criteria.pwdLength is between 8 and 128 characters long and stops the function generatePassword
+  // if criteria.pwdLength is smaller than 8 or bigger than 128.
+  if (criteria.pwdLength < 8) {
     alert("The password must be at least 8 characters long.");
     return;
-  } else if (criteria.length > 128) {
+  } else if (criteria.pwdLength > 128) {
     alert("The password must not be over 128 characters long.")
+    return;
+  } else if (!/\d/.test(criteria.pwdLength)) {
+    alert("Password does not contain a number.");
     return;
   }
   // The reason I didn't get all the values from the user in one block of code is because they will have to set all the values before the code
   // will check if anything is wrong, this way the program will give the user an error immediately after a wrong length value is input.
 
-  // This sets the rest of the values after criteria.length has been checked. 
+  // This sets the rest of the values after criteria.pwdLength has been checked. 
     criteria = {
-    length: criteria.length,
+    length: criteria.pwdLength,
     lowerCase: confirm("Include lowercase characters in the password?"),
     upperCase: confirm("Include uppercase characters in the password?"),
     numbers: confirm("Include numbers in the password?"),
@@ -45,21 +51,37 @@ function generatePassword() {
       return;
   }
 
-  // make an if statement with for loop that randomly generates a number based on how many chartypes were selected (if more than one)
-  var numCharTypes = 0;
-  
-  if (criteria.lowerCase == true) {
-    numCharTypes++;
-  } if (criteria.upperCase == true) {
-    numCharTypes++;
-  } if (criteria.numbers == true) {
-    numCharTypes++;
-  } if (criteria.special == true) {
-    numCharTypes++;
-  }
-
+  // var numCharTypes = -1;
+  // if (criteria.lowerCase == true) {
+  //   numCharTypes++;
+  // } if (criteria.upperCase == true) {
+  //   numCharTypes++;
+  // } if (criteria.numbers == true) {
+  //   numCharTypes++;
+  // } if (criteria.special == true) {
+  //   numCharTypes++;
+  // }
+  // var charType = Math.floor(Math.random() * numCharTypes);
+  // var charPicker = 0;
+  // for (var i = 0; i < criteria.length; i++) {
+  //   if (charType == 0) {
+  //     charPicker = Math.floor(Math.random() * lowIndex.length);
+  //     password += lowIndex[charPicker];
+  //   } else if (charType == 1) {
+  //     charPicker = Math.floor(Math.random() * upIndex.length);
+  //     password += upIndex[charPicker];
+  //   } else if (charType == 2) {
+  //     charPicker = Math.floor(Math.random() * numIndex.length);
+  //     password += numIndex[charPicker];
+  //   } else if (charType == 3) {
+  //     charPicker = Math.floor(Math.random() * spIndex.length);
+  //     password += spIndex[charPicker];
+  //   }
+  // }  
+  console.log(password);
+  // console.log(charType);
   console.log(criteria);
-  console.log(numCharTypes);
+  // console.log(numCharTypes);
 };
 
 
