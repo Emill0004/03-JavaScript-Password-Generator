05b46ef6090cf39df45fc678d505b01fdeb40ddf
Emill0004 <e.s.miller13125@gmail.com>
2023-03-10 23:22:31 -0700

    deleted script2.js and copied its code over to script.js, and removed the console.log tests.

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // Assignment Code
-// These four indexes contain all of the characters that the random number generator will pick to use in the password.
 var lowIndex = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q",
 "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
@@ -11,83 +10,63 @@ var numIndex = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var spIndex = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "_", "-", "+", "=", "`", "|", "/", "(", ")",
 "{", "}", "[", "]", ":", ";", "<", ">", ",", ".", "?"];
 
-
+var typeIndex = ["lower", "upper", "number", "special"];
 
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword() {
-  // This declares the varible criteria that will hold the various conditions the password needs to meet.
-  var password = "";
-  var criteria = {
-    pwdLength: prompt("Enter password length. (must be between 8 and 128 characters long)")
-  }
-  // This if statement checks if criteria.pwdLength is between 8 and 128 characters long and stops the function generatePassword
-  // if criteria.pwdLength is smaller than 8 or bigger than 128.
-  if (criteria.pwdLength < 8) {
-    alert("The password must be at least 8 characters long.");
-    return;
-  } else if (criteria.pwdLength > 128) {
-    alert("The password must not be over 128 characters long.")
-    return;
-  } else if (!/\d/.test(criteria.pwdLength)) {
-    alert("Password does not contain a number.");
-    return;
-  }
-  // The reason I didn't get all the values from the user in one block of code is because they will have to set all the values before the code
-  // will check if anything is wrong, this way the program will give the user an error immediately after a wrong length value is input.
-
-  // This sets the rest of the values after criteria.pwdLength has been checked. 
-    criteria = {
-    length: criteria.pwdLength,
-    lowerCase: confirm("Include lowercase characters in the password?"),
-    upperCase: confirm("Include uppercase characters in the password?"),
-    numbers: confirm("Include numbers in the password?"),
-    special: confirm("Include special characters in the password?")
-  }
-  // This if statement gives the user an error if none of the character types are set to true.
-  if (criteria.lowerCase !== true && criteria.upperCase !== true && 
-    criteria.numbers !== true && criteria.special !== true) {
-      alert("At least one type of character needs to be selected.");
-      return;
-  }
-
-  // var numCharTypes = -1;
-  // if (criteria.lowerCase == true) {
-  //   numCharTypes++;
-  // } if (criteria.upperCase == true) {
-  //   numCharTypes++;
-  // } if (criteria.numbers == true) {
-  //   numCharTypes++;
-  // } if (criteria.special == true) {
-  //   numCharTypes++;
-  // }
-  // var charType = Math.floor(Math.random() * numCharTypes);
-  // var charPicker = 0;
-  // for (var i = 0; i < criteria.length; i++) {
-  //   if (charType == 0) {
-  //     charPicker = Math.floor(Math.random() * lowIndex.length);
-  //     password += lowIndex[charPicker];
-  //   } else if (charType == 1) {
-  //     charPicker = Math.floor(Math.random() * upIndex.length);
-  //     password += upIndex[charPicker];
-  //   } else if (charType == 2) {
-  //     charPicker = Math.floor(Math.random() * numIndex.length);
-  //     password += numIndex[charPicker];
-  //   } else if (charType == 3) {
-  //     charPicker = Math.floor(Math.random() * spIndex.length);
-  //     password += spIndex[charPicker];
-  //   }
-  // }  
-  console.log(password);
-  // console.log(charType);
-  console.log(criteria);
-  // console.log(numCharTypes);
-};
-
-
-
 // Write password to the #password input
 function writePassword() {
+    function generatePassword() {
+        var pwdLength = prompt("Enter password length. (must be between 8 and 128 characters long)");
+        if (pwdLength < 8) {
+            alert("The password must be at least 8 characters long.");
+            return;
+        } else if (pwdLength > 128) {
+            alert("The password must not be over 128 characters long.")
+            return;
+        } else if (!/\d/.test(pwdLength)) {
+            alert("Password does not contain a number.");
+            return;
+        }
+        var charTypes = [confirm("Include lowercase characters in the password?")];
+        charTypes.push(confirm("Include uppercase characters in the password?"));
+        charTypes.push(confirm("Include numbers in the password?"));
+        charTypes.push(confirm("Include special characters in the password?"));
+        if (charTypes[0] !== true && charTypes[1] !== true && 
+            charTypes[2] !== true && charTypes[3] !== true) {
+            alert("At least one type of character needs to be selected.");
+            return;
+        }
+        for (var i = 3; i > -1; i--) {
+            if (charTypes[i] == true) {
+                charTypes[i] = typeIndex[i];
+            } else {
+                charTypes.splice(i, 1);
+            }
+        }
+        
+        var randomType;
+        var randomChar;
+        var protoPassword = "";
+        for (var i = 0; i < pwdLength; i++) {
+            randomType = Math.floor(Math.random() * charTypes.length);
+            if (charTypes[randomType] == "lower") {
+                randomChar = Math.floor(Math.random() * lowIndex.length);
+                protoPassword += lowIndex[randomChar];
+            } else if (charTypes[randomType] == "upper") {
+                randomChar = Math.floor(Math.random() * upIndex.length);
+                protoPassword += upIndex[randomChar];
+            } else if (charTypes[randomType] == "number") {
+                randomChar = Math.floor(Math.random() * numIndex.length);
+                protoPassword += numIndex[randomChar];
+            } else if (charTypes[randomType] == "special") {
+                randomChar = Math.floor(Math.random() * spIndex.length);
+                protoPassword += spIndex[randomChar];
+            }
+            
+        }
+        return protoPassword;
+    }
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
